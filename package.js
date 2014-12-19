18086ac86009995ac8d9fcac60a0ac997baebc22
Roshan Jobanputra <rosh93@gmail.com>
2014-12-19 13:53:20 -0800

    Upgrade to latest aws-sdk.

--- package.js
+++ package.js
@@ -1,15 +1,14 @@
 Package.describe({
   summary: "Wrapper for npm AWS SDK",
-  version: "0.0.1",
+  version: "0.0.2",
   git: "https://github.com/rosh93/meteor-aws-sdk"
 });
 
 Npm.depends({
-  'aws-sdk': '2.0.15'
+  'aws-sdk': '2.1.4'
 });
 
 Package.onUse(function(api) {
   api.add_files(['aws-sdk.js'], 'server');
   api.export('AWS', 'server');
-  api.versionsFrom('METEOR@0.9.0');
 });
